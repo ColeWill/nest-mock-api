f7a2709eb315d0e08a6be6a56d67614dfe831b01
cowilliams_deloitte <cowilliams@deloitte.com>
2024-12-02 11:19:48 -0700

    setting up for heroku

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Please navigate to /graphql or /api!ga .';
+    return 'Please navigate to /graphql or /api!.';
   }
 }
